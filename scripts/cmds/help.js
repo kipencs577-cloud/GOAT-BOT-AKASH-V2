c7fd561497967374bcf90eb810f23c673d74e345
kipencs577-cloud <kipencs577@gmail.com>
2026-01-21 19:44:44 +0300

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -7,7 +7,7 @@ module.exports = {
     name: "help",
     aliases: ["menu", "commands"],
     version: "5.0",
-    author: "AKASH",
+    author: "ncs",
     shortDescription: "Show all commands",
     longDescription: "Show all commands in fancy font with boxes",
     category: "system",
@@ -65,7 +65,7 @@ module.exports = {
     let msg = `│\n│  ${fancyFont("COMMANDS MENU")}\n│  ───────────────\n`;
     msg += `│  ${fancyFont("PREFIX")} : ${prefix}\n`;
     msg += `│  ${fancyFont("TOTAL")}  : ${allCommands.size}\n`;
-    msg += `│  ${fancyFont("AUTHOR")} : AKASH\n│\n`;
+    msg += `│  ${fancyFont("AUTHOR")} : ncs pro\n│\n`;
 
     for (const cat of Object.keys(categories)) {
       msg += `│  ┌─ ${categoryFont(cat.toUpperCase())} ─┐\n`;
